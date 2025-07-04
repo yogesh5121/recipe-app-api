96f62f767d0964f0877903ad64ec157b6b76e138
yogesh <yogeshthanjai@gmail.com>
2025-07-04 11:43:52 +0530

    configured docker and wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+
+"""
+calculator.py
+"""
+def add(x, y):
+    """Add two numbers."""
+    return x + y
+def subtract(x, y):
+    """Subtract two numbers."""
+    return abs(x-y)
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,23 @@
+
+import time
+from psycopg2 import OperationalError as Psycopg2OperationalError
+from django.db.utils import OperationalError
+from typing import Any
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to wait for the database to be available."""
+
+    def handle(self, *args: Any, **options: Any):
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OperationalError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
