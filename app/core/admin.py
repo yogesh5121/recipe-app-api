1be95c8d206839a6ffa6ac8d7f1e9d1a23291589
yogesh <yogeshthanjai@gmail.com>
2025-07-10 11:56:53 +0530

    Implemeted  User API

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from core import models
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(models.User, UserAdmin)
